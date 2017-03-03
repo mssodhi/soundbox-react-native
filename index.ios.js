1390e91e3d645fbe146973eafa384a8383a82ff2
Manu SOdhi <mssodhi@ucdavis.edu>
2017-03-02 19:49:47 -0800

    more cleanup

--- index.ios.js
+++ index.ios.js
@@ -1,18 +1,11 @@
-import { AppRegistry, View } from 'react-native';
-import React, { Component } from 'react';
+import React from 'react';
+import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 
 import configureStore from './src/shared/reducers/configureStore'
 import App from './src/app/App';
 
 const store = configureStore();
-
-class Main extends Component {
-  render() {
-  	return (
-  	<Provider store={store}><App /></Provider>
-  	);
-  }
-}
+const Main = () => <Provider store={store}><App /></Provider>
 
 AppRegistry.registerComponent('soundboxNative', () => Main);
