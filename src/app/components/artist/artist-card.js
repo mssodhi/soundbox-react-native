0d581fa7d94d4eda16724754e21ca2c9fab5f7ba
Manu SOdhi <mssodhi@ucdavis.edu>
2017-03-19 10:00:17 -0700

    wip

--- src/app/components/artist/artist-card.js
+++ src/app/components/artist/artist-card.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Dimensions } from 'react-native';
 const { height, width } = Dimensions.get('window');
-import { StyleSheet, View, Text, Image } from 'react-native'
+import { StyleSheet, View, Text, Image, TouchableHighlight } from 'react-native'
 const noImage = require('../../../shared/images/Wind-Vector-resize.png')
 
 export default class ArtistCard extends React.Component {
   constructor(props) {
     super(props)
+  }
+
+  handleFavorite() {
+    console.log('toggle fav');
+  }
+
+  handleShare() {
+    console.log('share');
   }
 
   render() {
@@ -15,6 +23,14 @@ export default class ArtistCard extends React.Component {
       <View style={styles.card}>
         <Image style={styles.backgroundImage} source={noImage} />
         <Image style={styles.avatar} source={imageUrl} />
+        <View style={styles.cardActions}>
+          <TouchableHighlight underlayColor={'#222'} style={styles.actions} onPress={() => this.handleFavorite()}>
+            <Text style={{ color: 'red' }}>Add to Favorites</Text>
+          </TouchableHighlight>
+          <TouchableHighlight underlayColor={'#222'} style={styles.actions} onPress={() => this.handleShare()}>
+            <Text style={{ color: 'red' }}>Share</Text>
+          </TouchableHighlight>
+        </View>
       </View>
     )
   }
@@ -23,7 +39,7 @@ export default class ArtistCard extends React.Component {
 const styles = StyleSheet.create({
   card: {
     backgroundColor: '#222',
-    height: 150,
+    height: 175,
     width: width - 20,
     margin: 10,
     borderRadius: 2,
@@ -41,5 +57,13 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     marginTop: -40,
     alignSelf: 'center'
+  },
+  cardActions: {
+    flex: 1,
+    justifyContent: 'space-between',
+    flexDirection: 'row'
+  },
+  actions: {
+    padding: 10
   }
 });
